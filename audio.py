ebcaa74567ca44b8972d0381ea1d874bf7cb2b18
WenlinMao <maowenlin1997@gmail.com>
2025-02-08 14:49:57 -0800

    add audio.py

--- audio.py
+++ audio.py
@@ -1,7 +1,7 @@
 import pygame
 
 pygame.mixer.init()
-pygame.mixer.music.load("test.mp3")  # Replace with your audio file
+pygame.mixer.music.load("dog.wav")  # Replace with your audio file
 pygame.mixer.music.play()
 
 while pygame.mixer.music.get_busy():
